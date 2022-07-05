45be3d28c94273ab8b89a2d7c81df49476136fa3
Schepa <sschepankevich@yandex.ru>
2022-07-05 20:51:44 +0700

    13. Add modules

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className='header'>
+      <img src='https://www.logaster.com/blog/wp-content/uploads/2020/03/the-4th-logo-1024x862.jpg'></img>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+function Content() {
+  return (
+    <div className='content'>
+      <div>
+        <img
+          className='content__header'
+          src='https://gratisography.com/wp-content/uploads/2022/06/gratisography-boom-box-free-stock-photo-1170x780.jpg'
+        />
+      </div>
+      <div>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Novosibirsk_Metro_logo.svg/97px-Novosibirsk_Metro_logo.svg.png' />
+      </div>
+      <div>My posts</div>
+      <div>New post</div>
+      <div>post 1</div>
+      <div>post 2</div>
+    </div>
+  );
+}
+
+export default Content;
