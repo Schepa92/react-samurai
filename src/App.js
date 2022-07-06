ef7925097a6a02cb6e99008ace9f885385268fcc
Schepa <sschepankevich@yandex.ru>
2022-07-06 22:05:53 +0700

    19. route

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import Profile from './components/Profile/Profile';
 import Header from './components/Header/Header';
 import NavBar from './components/Navbar/Navbar';
 import Dialogs from './components/Dialogs/Dialogs';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import News from './components/News/News';
 
-function App() {
+const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <NavBar />
-      <div className='app-wrapper__content'>
-        {/* <Profile /> */}
-        <Dialogs />
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <NavBar />
+        <div className='app-wrapper__content'>
+          <Routes>
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/dialogs' element={<Dialogs />} />
+            <Route path='/news' element={<News />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
