fae4e5c38ccc8d97694e15bdb85ee113cc8f6e3e
Schepa <sschepankevich@yandex.ru>
2022-07-06 21:19:56 +0700

    18

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import Profile from './components/Profile/Profile';
 import Header from './components/Header/Header';
 import NavBar from './components/Navbar/Navbar';
+import Dialogs from './components/Dialogs/Dialogs';
 
 function App() {
   return (
     <div className='app-wrapper'>
       <Header />
       <NavBar />
-      <Profile />
+      <div className='app-wrapper__content'>
+        {/* <Profile /> */}
+        <Dialogs />
+      </div>
     </div>
   );
 }
