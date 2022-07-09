048a38691a7c957d1893088745a82d6668fdc7e6
Schepa <sschepankevich@yandex.ru>
2022-07-09 11:27:58 +0700

    25. array.map

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,26 +1,58 @@
 import s from './Dialogs.module.css';
-import { useParams } from 'react-router-dom';
 import Dialog from './Dialog/Dialog';
 import Message from './Message/Message';
 
 const Dialogs = (props) => {
-  // console.log(useParams());
+  let dialogsData = [
+    {
+      name: 'Sergey',
+      id: 1,
+    },
+    {
+      name: 'Andrew',
+      id: 2,
+    },
+    {
+      name: 'Alex',
+      id: 3,
+    },
+    {
+      name: 'Vlad',
+      id: 4,
+    },
+  ];
+
+  let messagesData = [
+    {
+      message: 'Hello',
+      id: 1,
+    },
+    {
+      message: 'Helweerefdlo',
+      id: 2,
+    },
+    {
+      message: 'Aldfsewex',
+      id: 3,
+    },
+    {
+      message: 'Vwefewflad',
+      id: 4,
+    },
+  ];
+
+  let dialogsElements = dialogsData.map((d) => (
+    <Dialog name={d.name} id={d.id} />
+  ));
+
+  let messagesElements = messagesData.map((m) => (
+    <Message message={m.message} id={m.id} />
+  ));
+
   return (
     <div className={s.dialogs}>
-      <div className={s.dialogs_items}>
-        <Dialog name='Sergey' id='1' />
-        <Dialog name='Andrey' id='2' />
-        <Dialog name='Alex' id='3' />
-        <Dialog name='Vlad' id='4' />
-        <Dialog name='Valera' id='5' />
-      </div>
-      <div className={s.messages}>
-        <Message message='Hello!' />
-        <Message message='Hello!21e2e' />
-        <Message message='Hellorrwee!' />
-        <Message message='Helweerefdlo!' />
-        <Message message='Heldsfdlo!' />
-      </div>
+      <div className={s.dialogs_items}>{dialogsElements}</div>
+      <div className={s.messages}>{messagesElements}</div>
     </div>
   );
 };
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,15 +2,29 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = (props) => {
+  let postData = [
+    {
+      id: 1,
+      message: 'How are you?',
+      likesCount: 12,
+    },
+    {
+      id: 2,
+      message: 'My friend',
+      likesCount: 72,
+    },
+  ];
+
+  let postsElements = postData.map((p) => (
+    <Post message={p.message} likesCount={p.likesCount} id={p.id} />
+  ));
+
   return (
     <div className={s.posts}>
       <h3>My posts</h3>
       <textarea></textarea>
       <button>Add post</button>
-      <div className={s.posts}>
-        <Post message='Hello!' likesCount='5' />
-        <Post message='How are you?' likesCount='38' />
-      </div>
+      <div className={s.posts}>{postsElements}</div>
     </div>
   );
 };
